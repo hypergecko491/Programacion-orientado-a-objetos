9d79710268c6d664c726d1268543cbe07140b54c
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-04 13:02:00 -0600

    Refactor ObjetoEstImc class and improve student storage

--- ObjetoEstImc.java
+++ ObjetoEstImc.java
@@ -2,37 +2,27 @@ package ArrayObjetos;
 import Metodos.JOption;
 import TDA.EstudianteIMC;
 public class ObjetoEstImc {
-static String unir = "Estudiantes registrados:\n\n";
-public static void MenuIMC() {
-        String Menu = "Agregar Estudiante,Visualizar,Salir";
-        byte op = JOption.BotonOpciones(Menu);
-        while (op != 2) {
-            switch (op) {
-                case 0:
-                    AgregarEstudiante();
-                    JOption.VerDatos("Estudiante agregado", unir);
-                    break;
-                case 1:
-                    JOption.VerDatos("Lista de estudiantes", unir);
-                    break;
-                default:
-                    JOption.VerDatos("Opción inválida", "");
-            }
-            op = JOption.BotonOpciones(Menu);
-        }
-    }
-    public static void AgregarEstudiante() {
+private EstudianteIMC datos;
+private byte j;
+public ObjetoEstImc(byte tam){
+    datos = new EstudianteIMC[tam];
+    j=0;
+}
+public boolean arrayVacio(){
+  return datos==null;    
+}
+public EstudianteIMC AlmacenarEstudiante(){
         EstudianteIMC obj = new EstudianteIMC();
         obj.setNombre(JOption.LecturaString("Nombre del estudiante"));
         obj.setNacimiento(JOption.LecturaShort("Año de nacimiento"));
         obj.setPeso(JOption.LecturaFloat("Peso en kg"));
         obj.setAltura(JOption.LecturaFloat("Altura en metros"));
-        unir += "\n Nombre: " + obj.getNombre()
-              + "\n Año nacimiento: " + obj.getNacimiento()
-              + "\n Peso: " + obj.getPeso()
-              + "\n Altura: " + obj.getAltura()
-              + "\n IMC: " + String.format("%.2f", obj.calcularIMC())
-              + "\n Clasificación: " + obj.clasificarIMC()
-              + "\n----------------------------------";
-    }
+        return obj;
+}
+public void AgregarEstudiante(){
+    if (j<datos.length-1){
+        datos[j]=AlmacenarEstudiante();
+        j++;
+    }else JOption.MostrarError("ya no hay espacio ");
+}
 }
