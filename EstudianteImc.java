7df8d6a1fd5d2541b62e1ef3daa63cb1b8cd710b
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-03 18:56:43 -0600

    Refactor EstudianteIMC class and add IMC methods

--- EstudianteImc.java
+++ EstudianteImc.java
@@ -1,27 +1,41 @@
 package TDA;
 public class EstudianteIMC {
-private String Nombre;
-private short Nacimiento;
-private float Peso;
-private float Altura;
+    private String Nombre;
+    private short Nacimiento;
+    private float Peso;
+    private float Altura;
     public EstudianteIMC() {}
     public EstudianteIMC(String Nombre, short Nacimiento, float Peso, float Altura) {
         this.Nombre = Nombre;
         this.Nacimiento = Nacimiento;
         this.Peso = Peso;
-        this.Altura = Altura;
-    }
+        this.Altura = Altura;}
     public String getNombre(){return Nombre;}
-    public void setNombre(String Nombre){this.Nombre = Nombre;}
+    public void setNombre(String Nombre){this.Nombre=Nombre;}
     public short getNacimiento(){return Nacimiento;}
-    public void setNacimiento(short Nacimiento){this.Nacimiento = Nacimiento;}
+    public void setNacimiento(short Nacimiento) { this.Nacimiento = Nacimiento; }
     public float getPeso(){return Peso;}
     public void setPeso(float Peso){this.Peso = Peso;}
     public float getAltura(){return Altura;}
-    public void setAltura(float Altura){this.Altura = Altura;    }
+    public void setAltura(float Altura){ this.Altura = Altura;}
+    public float calcularIMC(){return (Peso /(Altura*Altura));}
+    public String clasificarIMC() {
+        float imc = calcularIMC();
+        if (imc < 18.5) {
+            return "Bajo peso";
+        } else if (imc < 25) {
+            return "Peso normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidad";
+        }
+    }
     @Override
     public String toString() {
-        return "EstudianteIMC{" + "Nombre=" + Nombre + ", Nacimiento=" + Nacimiento + ", Peso=" + Peso + ", Altura=" + Altura + '}';
+        return "Nombre: " + Nombre +"\nNacimiento: " + Nacimiento +
+               "\nPeso: " + Peso +"\nAltura: " + Altura +"\nIMC: "+ 
+                String.format("%.2f", calcularIMC()) +
+               "\nClasificación: " + clasificarIMC();
     }
-
 }
