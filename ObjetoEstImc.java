d7db5b7d07e35bc011ac225ab15ac43905cff680
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-05 07:43:22 -0600

    Refactor ObjetoEstImc class and methods

--- ObjetoEstImc.java
+++ ObjetoEstImc.java
@@ -2,27 +2,25 @@ package ArrayObjetos;
 import Metodos.JOption;
 import TDA.EstudianteIMC;
 public class ObjetoEstImc {
-private EstudianteIMC datos;
-private byte j;
-public ObjetoEstImc(byte tam){
-    datos = new EstudianteIMC[tam];
-    j=0;
-}
-public boolean arrayVacio(){
-  return datos==null;    
-}
-public EstudianteIMC AlmacenarEstudiante(){
+    private EstudianteIMC[] datos;
+    private byte j;
+    public ObjetoEstImc(int tam){
+        datos = new EstudianteIMC[tam];
+        j = 0;}
+    public boolean arrayVacio(){
+        return j == 0;}
+    public EstudianteIMC AlmacenarEstudiante(){
         EstudianteIMC obj = new EstudianteIMC();
         obj.setNombre(JOption.LecturaString("Nombre del estudiante"));
         obj.setNacimiento(JOption.LecturaShort("Año de nacimiento"));
         obj.setPeso(JOption.LecturaFloat("Peso en kg"));
         obj.setAltura(JOption.LecturaFloat("Altura en metros"));
-        return obj;
-}
-public void AgregarEstudiante(){
-    if (j<datos.length-1){
-        datos[j]=AlmacenarEstudiante();
-        j++;
-    }else JOption.MostrarError("ya no hay espacio ");
-}
+        return obj;}
+    public void AgregarEstudiante(){
+        if (j < datos.length){
+            datos[j] = AlmacenarEstudiante();
+            j++;
+        }else{
+            JOption.MostrarError("Ya no hay espacio");}
+    }
 }
