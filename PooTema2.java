3713cdc660ead9e3c3e23fc605c00fb480e7e841
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-06 12:37:07 -0600

    Add sorting option to IMC menu

--- PooTema2.java
+++ PooTema2.java
@@ -8,11 +8,11 @@ public class PooTema2 {
     }
 public static void MenuIMC() {
     
-        String Menu = "Agregar, Consulta general, Salir";
+        String Menu = "Agregar, Consulta general, ordenar datos, Salir";
         byte tam=EntradaSalida.LecturaByte("cuantos estudiantes son?");
         ObjetoEstImc lista=new ObjetoEstImc(tam);
         byte op = EntradaSalida.BotonOpciones(Menu);
-        while (op != 2) {
+        while (op != 3) {
             switch (op) {
                 case 0:
                     lista.AgregarEstudiante();
@@ -21,6 +21,11 @@ public static void MenuIMC() {
                 case 1:
                     EntradaSalida.VerDatos("Calculo del Imc de estudiantes", unir + lista.VerObjetos());
                     break;
+                case 2: String cad="Datos desordenados:\n"+lista.VerObjetos();
+                lista.OrdenarBubble();
+                cad+="Datos ordenados: \n "+lista.VerObjetos();
+                EntradaSalida.VerDatos("calculo de imc de estudiante", cad);
+                break;
                 default:
                     EntradaSalida.VerDatos("Opción inválida", "");
             }
