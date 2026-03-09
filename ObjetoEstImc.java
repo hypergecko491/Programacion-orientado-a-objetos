2b8f4efe963109565a5f22ce63c511b8c2a93dba
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-09 12:49:01 -0600

    Refactor ObjetoEstImc methods and sorting logic

--- ObjetoEstImc.java
+++ ObjetoEstImc.java
@@ -2,28 +2,28 @@ package ArrayObjetos;
 import Metodos.EntradaSalida;
 import TDA.EstudianteIMC;
 public class ObjetoEstImc {
-    private EstudianteIMC datos[];
-    private byte j;
-    public ObjetoEstImc(int tam){
+private EstudianteIMC datos[];
+private byte j;
+public ObjetoEstImc(int tam){
         datos = new EstudianteIMC[tam];
         j = 0;}
-    public boolean arrayVacio(){
+public boolean arrayVacio(){
         return j == 0;}
-    public EstudianteIMC AlmacenarEstudiante(){
+public EstudianteIMC AlmacenarEstudiante(){
         EstudianteIMC obj = new EstudianteIMC();
         obj.setNombre(EntradaSalida.LecturaString("Nombre del estudiante"));
         obj.setNacimiento(EntradaSalida.LecturaShort("Año de nacimiento"));
         obj.setPeso(EntradaSalida.LecturaFloat("Peso en kg"));
         obj.setAltura(EntradaSalida.LecturaFloat("Altura en metros"));
         return obj;}
-    public void AgregarEstudiante(){
+public void AgregarEstudiante(){
         if (j < datos.length){
             datos[j] = AlmacenarEstudiante();
             j++;
         }else{
             EntradaSalida.MostrarError("Ya no hay espacio");}
     }
-    public String VerObjetos(){
+public String VerObjetos(){
         String cad="";
         for(byte k=0;k<j;k++)
             cad+= (k+1)+")"+datos[k].toString()+"\n";
@@ -31,12 +31,19 @@ public class ObjetoEstImc {
     }
 public void OrdenarBubble(){
     EstudianteIMC aux;
-    byte i,k;
-    for(i=0;i<datos.length-1;i++){
-        for(k=(byte)(i+1);k<datos.length;k++){
-            if(datos[i].getNacimiento()>datos[k].getNacimiento()){
-            aux=datos[i];
-            datos[i]=datos[k];
-            datos[k]=aux;}}}
+
+    for(int i=0; i<j-1; i++){
+        for(int k=0; k<j-1-i; k++){
+
+            if(datos[k].getNacimiento() > datos[k+1].getNacimiento()){
+
+                aux = datos[k];
+                datos[k] = datos[k+1];
+                datos[k+1] = aux;
+
+            }
+
         }
+    }
+}
 }
