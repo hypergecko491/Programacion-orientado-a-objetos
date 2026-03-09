de17d74261dcc7535e6ab8b59560b5d913084126
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-09 12:47:52 -0600

    Update EstudianteImc.java

--- EstudianteImc.java
+++ EstudianteImc.java
@@ -1,25 +1,25 @@
 package TDA;
 public class EstudianteIMC {
-    private String Nombre;
-    private short Nacimiento;
-    private float Peso;
-    private float Altura;
-    public EstudianteIMC() {}
-    public EstudianteIMC(String Nombre, short Nacimiento, float Peso, float Altura) {
+private String Nombre;
+private short Nacimiento;
+private float Peso;
+private float Altura;
+public EstudianteIMC() {}
+public EstudianteIMC(String Nombre, short Nacimiento, float Peso, float Altura) {
         this.Nombre = Nombre;
         this.Nacimiento = Nacimiento;
         this.Peso = Peso;
         this.Altura = Altura;}
-    public String getNombre(){return Nombre;}
-    public void setNombre(String Nombre){this.Nombre=Nombre;}
-    public short getNacimiento(){return Nacimiento;}
-    public void setNacimiento(short Nacimiento) { this.Nacimiento = Nacimiento; }
-    public float getPeso(){return Peso;}
-    public void setPeso(float Peso){this.Peso = Peso;}
-    public float getAltura(){return Altura;}
-    public void setAltura(float Altura){ this.Altura = Altura;}
-    public float calcularIMC(){return (Peso /(Altura*Altura));}
-    public String clasificarIMC() {
+public String getNombre(){return Nombre;}
+public void setNombre(String Nombre){this.Nombre=Nombre;}
+public short getNacimiento(){return Nacimiento;}
+public void setNacimiento(short Nacimiento) { this.Nacimiento = Nacimiento; }
+public float getPeso(){return Peso;}
+public void setPeso(float Peso){this.Peso = Peso;}
+public float getAltura(){return Altura;}
+public void setAltura(float Altura){ this.Altura = Altura;}
+public float calcularIMC(){return (Peso /(Altura*Altura));}
+public String clasificarIMC() {
         float imc = calcularIMC();
         if (imc < 18.5) {
             return "Bajo peso";
@@ -31,8 +31,8 @@ public class EstudianteIMC {
             return "Obesidad";
         }
     }
-    @Override
-    public String toString() {
+@Override
+public String toString() {
         return "Nombre: " + Nombre +"\nNacimiento: " + Nacimiento +
                "\nPeso: " + Peso +"\nAltura: " + Altura +"\nIMC: "+ 
                 String.format("%.2f", calcularIMC()) +
