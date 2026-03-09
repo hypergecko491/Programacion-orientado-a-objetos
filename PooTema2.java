5404179ef22cde0f0b35299bbcb3e58bb9820cb3
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-09 12:52:06 -0600

    Refactor output methods for student data

--- PooTema2.java
+++ PooTema2.java
@@ -2,12 +2,11 @@ package com.mycompany.progratema2;
 import Metodos.EntradaSalida;
 import ArrayObjetos.ObjetoEstImc;
 public class PooTema2 {
-    static String unir = "Estudiantes registrados:\n\n";
-    public static void main(String[] args) {
+static String unir = "Estudiantes registrados:\n\n";
+public static void main(String[] args) {
         MenuIMC();
     }
 public static void MenuIMC() {
-    
         String Menu = "Agregar, Consulta general, ordenar datos, Salir";
         byte tam=EntradaSalida.LecturaByte("cuantos estudiantes son?");
         ObjetoEstImc lista=new ObjetoEstImc(tam);
@@ -16,18 +15,18 @@ public static void MenuIMC() {
             switch (op) {
                 case 0:
                     lista.AgregarEstudiante();
-                    EntradaSalida.VerDatos("Estudiante agregado", unir + lista.VerObjetos());
+                    EntradaSalida.VerDatos2("Estudiante agregado", unir + lista.VerObjetos());
                     break;
                 case 1:
-                    EntradaSalida.VerDatos("Calculo del Imc de estudiantes", unir + lista.VerObjetos());
+                    EntradaSalida.VerDatos2("Calculo del Imc de estudiantes", unir + lista.VerObjetos());
                     break;
                 case 2: String cad="Datos desordenados:\n"+lista.VerObjetos();
                 lista.OrdenarBubble();
                 cad+="Datos ordenados: \n "+lista.VerObjetos();
-                EntradaSalida.VerDatos("calculo de imc de estudiante", cad);
+                EntradaSalida.VerDatos2("calculo de imc de estudiante", cad);
                 break;
                 default:
-                    EntradaSalida.VerDatos("Opción inválida", "");
+                    EntradaSalida.VerDatos2("Opción inválida", "");
             }
             op = EntradaSalida.BotonOpciones(Menu);
         }
