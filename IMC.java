f248c7670f79ee282aa7e95e03c43cd9a6e2c4db
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-03-12 14:20:01 -0600

    Menu IMC poo tema 2

---
+++ IMC.java
@@ -0,0 +1,32 @@
+package Menus;
+import ArrayObjetos.ObjetoEstImc;
+import Metodos.EntradaSalida;
+public class IMC {
+public static void MenuIMC(){
+    String unir = "Estudiantes registrados:\n\n";
+    String Menu = "Agregar, Consulta general, ordenar datos, Salir";
+    byte tam = EntradaSalida.LecturaByte("cuantos estudiantes son?");
+    ObjetoEstImc lista = new ObjetoEstImc(tam);
+    byte op = EntradaSalida.BotonOpciones(Menu);
+    while (op != 3) {
+        switch (op) {
+            case 0:
+                lista.AgregarEstudiante();
+                EntradaSalida.VerDatos2("Estudiante agregado", unir + lista.VerObjetos());
+            break;
+            case 1:
+                EntradaSalida.VerDatos2("Calculo del Imc de estudiantes", unir + lista.VerObjetos());
+            break;
+            case 2:
+                String cad = "Datos desordenados:\n" + lista.VerObjetos();
+                lista.OrdenarBubble();
+                cad += "Datos ordenados:\n" + lista.VerObjetos();
+                EntradaSalida.VerDatos2("calculo de imc de estudiante", cad);
+            break;
+            default:
+                EntradaSalida.VerDatos2("Opción inválida", "");
+        }
+        op = EntradaSalida.BotonOpciones(Menu);
+    }
+}
+}
