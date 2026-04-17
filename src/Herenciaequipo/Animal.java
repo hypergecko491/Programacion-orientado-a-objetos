d8c406615e8eaed688b70f038a2d6a17b497c353
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 17:59:46 -0600

    Clase Animal(padre)

---
+++ src/Herenciaequipo/Animal.java
@@ -0,0 +1,37 @@
+package Herenciaequipo;
+public class Animal {
+protected String Nombre;
+protected byte Edad;
+protected float Peso;
+protected String Dieta;
+protected String HorarioAlimentacion;
+protected String Sonido;
+public Animal(){}
+public Animal(String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Peso = Peso;
+        this.Dieta = Dieta;
+        this.HorarioAlimentacion = HorarioAlimentacion;
+        this.Sonido = Sonido;
+    }
+public String getNombre(){return Nombre;}
+public void setNombre(String Nombre){this.Nombre = Nombre;}
+public byte getEdad(){return Edad;}
+public void setEdad(byte Edad){this.Edad = Edad;}
+public float getPeso(){return Peso;}
+public void setPeso(float Peso){this.Peso = Peso;}
+public String getDieta(){return Dieta;}
+public void setDieta(String Dieta){this.Dieta = Dieta;}
+public String getHorarioAlimentacion(){return HorarioAlimentacion;}
+public void setHorarioAlimentacion(String HorarioAlimentacion){this.HorarioAlimentacion = HorarioAlimentacion;}
+public String getSonido(){return Sonido;}
+public void setSonido(String Sonido) {this.Sonido = Sonido;}
+@Override
+public String toString() {
+        return "Animal{" + "Nombre=" + Nombre + ",\nEdad=" + Edad +
+               ", \nPeso=" + Peso + ", \nDieta=" + Dieta +
+               ", \nHorarioAlimentacion=" + HorarioAlimentacion +
+               ",\n Sonido:"+ Sonido+'}';
+    }
+}
