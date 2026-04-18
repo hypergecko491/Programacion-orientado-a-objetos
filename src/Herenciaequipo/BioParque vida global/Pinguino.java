a314df165cd375c20e93d05bc80ffe3b11070ce5
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:51:39 -0600

    Refactor Pinguino constructor parameters order

--- src/Herenciaequipo/BioParque vida global/Pinguino.java
+++ src/Herenciaequipo/BioParque vida global/Pinguino.java
@@ -3,18 +3,20 @@ public class Pinguino extends Ave {
     private float VelocidadNado;
     private String Tipo;
     public Pinguino() {}
-    public Pinguino(float EnvergaduraAlas,
-                    String Nombre, byte Edad, float Peso,
+    public Pinguino(String Nombre, byte Edad, float Peso,
                     String Dieta, String HorarioAlimentacion, String Sonido,
+                    float EnvergaduraAlas,
                     float VelocidadNado, String Tipo) {
-        super(EnvergaduraAlas, Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
+        super(Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido, EnvergaduraAlas);
         this.VelocidadNado = VelocidadNado;
         this.Tipo = Tipo;
     }
     public float getVelocidadNado(){return VelocidadNado;}
     public void setVelocidadNado(float VelocidadNado){this.VelocidadNado = VelocidadNado;}
+
     public String getTipo(){return Tipo;}
     public void setTipo(String Tipo){this.Tipo = Tipo;}
+
     @Override
     public String toString() {
         return "PINGUINO\n" + super.toString() +
