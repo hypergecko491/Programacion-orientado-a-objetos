623ab72eb9d78708f39a657cc2fd146cdc138b6b
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:39:15 -0600

    Refactor Aguila class constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Aguila.java
+++ src/Herenciaequipo/BioParque vida global/Aguila.java
@@ -1,24 +1,25 @@
 package Herenciaequipo;
+
 public class Aguila extends Ave {
-private float AgudezaVisual;
-private float VelocidadVuelo;
-public Aguila(){}
-public Aguila(float AgudezaVisual, float VelocidadVuelo, float EnvergaduraAlas, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+
+    private float VelocidadVuelo;
+    private float AgudezaVisual;
+
+    public Aguila(float EnvergaduraAlas,
+                  String Nombre, byte Edad, float Peso,
+                  String Dieta, String HorarioAlimentacion, String Sonido,
+                  float VelocidadVuelo, float AgudezaVisual) {
+
         super(EnvergaduraAlas, Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
-        this.AgudezaVisual = AgudezaVisual;
+
         this.VelocidadVuelo = VelocidadVuelo;
-    }
-public float getAgudezaVisual() {return AgudezaVisual;}
-public void setAgudezaVisual(float AgudezaVisual) {this.AgudezaVisual = AgudezaVisual;}
-public float getVelocidadVuelo(){return VelocidadVuelo;}
-public void setVelocidadVuelo(float VelocidadVuelo){this.VelocidadVuelo = VelocidadVuelo;}
-    @Override
-    public String toString() {
-        return "Aguila{" +"Nombre=" + Nombre + ", \nEdad=" + Edad +
-           ",\nPeso=" + Peso + ", \nDieta=" + Dieta + ", "+
-           ",\nHorarioAlimentacion=" + HorarioAlimentacion + ",\nSonido:"+Sonido+
-           ",\nEnvergaduraAlas=" + EnvergaduraAlas+ ",\nAgudezaVisual=" + AgudezaVisual +
-           ",\nVelocidadVuelo=" + VelocidadVuelo + '}';
+        this.AgudezaVisual = AgudezaVisual;
     }
 
+    @Override
+    public String toString() {
+        return "AGUILA\n" + super.toString() +
+               "\nVelocidadVuelo=" + VelocidadVuelo +
+               "\nAgudezaVisual=" + AgudezaVisual + "\n";
+    }
 }
