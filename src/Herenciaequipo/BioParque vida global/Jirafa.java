1071018b22331202e224637ed3a9539b66af10c2
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:47:52 -0600

    Refactor Jirafa constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Jirafa.java
+++ src/Herenciaequipo/BioParque vida global/Jirafa.java
@@ -1,24 +1,24 @@
 package Herenciaequipo;
 public class Jirafa extends Mamifero {
-private float LongitudCuello;
-private float Altura;
-public Jirafa() {}
-public Jirafa(float LongitudCuello, float Altura, String TipoPelaje, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
-        super(TipoPelaje, Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
+    private float LongitudCuello;
+    private float Altura;
+    public Jirafa() {}
+    public Jirafa(String Nombre, byte Edad, float Peso,
+                  String Dieta, String HorarioAlimentacion, String Sonido,
+                  String TipoPelaje,
+                  float LongitudCuello, float Altura) {
+        super(Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido, TipoPelaje);
         this.LongitudCuello = LongitudCuello;
         this.Altura = Altura;
     }
-public float getLongitudCuello(){return LongitudCuello;}
-public void setLongitudCuello(float LongitudCuello) {this.LongitudCuello = LongitudCuello;}
-public float getAltura(){return Altura;}
-public void setAltura(float Altura){this.Altura=Altura;}
-@Override
-public String toString() {
-        return "Jirafa{"+"Nombre=" + Nombre + ", \nEdad=" + Edad +
-           ",\nPeso=" + Peso + ", \nDieta=" + Dieta +
-           ",\nHorarioAlimentacion=" + HorarioAlimentacion+
-           ",\nSonido"+Sonido + ",\nTipoPelaje=" + TipoPelaje+
-           ",\nLongitudCuello="+LongitudCuello+",\nAltura="+Altura+ '}';
+    public float getLongitudCuello(){return LongitudCuello;}
+    public void setLongitudCuello(float LongitudCuello){this.LongitudCuello = LongitudCuello;}
+    public float getAltura(){return Altura;}
+    public void setAltura(float Altura){this.Altura = Altura;}
+    @Override
+    public String toString() {
+        return "JIRAFA\n" + super.toString() +
+               "\nLongitudCuello=" + LongitudCuello +
+               "\nAltura=" + Altura + "\n";
     }
-
 }
