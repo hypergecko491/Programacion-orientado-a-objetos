a01f6df0e268a7246f8ad254d90dff4919e3f9e1
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:42:20 -0600

    Refactor Pinguino constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Pinguino.java
+++ src/Herenciaequipo/BioParque vida global/Pinguino.java
@@ -1,23 +1,24 @@
 package Herenciaequipo;
 public class Pinguino extends Ave {
-private float VelocidadNado;
-private String Tipo;
+    private float VelocidadNado;
+    private String Tipo;
     public Pinguino() {}
-    public Pinguino(float VelocidadNado, String Tipo, float EnvergaduraAlas, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+    public Pinguino(float EnvergaduraAlas,
+                    String Nombre, byte Edad, float Peso,
+                    String Dieta, String HorarioAlimentacion, String Sonido,
+                    float VelocidadNado, String Tipo) {
         super(EnvergaduraAlas, Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
         this.VelocidadNado = VelocidadNado;
-        this.Tipo = Tipo;}
+        this.Tipo = Tipo;
+    }
     public float getVelocidadNado(){return VelocidadNado;}
     public void setVelocidadNado(float VelocidadNado){this.VelocidadNado = VelocidadNado;}
     public String getTipo(){return Tipo;}
-    public void setTipo(String Tipo) {this.Tipo = Tipo;}
+    public void setTipo(String Tipo){this.Tipo = Tipo;}
     @Override
     public String toString() {
-        return "Pinguino{" +"Nombre=" + Nombre + ", \nEdad=" + Edad +
-           ",\nPeso=" + Peso + ", \nDieta=" + Dieta + ", "+
-           ",\nHorarioAlimentacion=" + HorarioAlimentacion + ",\nSonido:"+Sonido+
-           ",\nEnvergaduraAlas=" + EnvergaduraAlas+ 
-           ",\nVelocidadNado=" + VelocidadNado + ",\nTipo=" + Tipo + '}';
+        return "PINGUINO\n" + super.toString() +
+               "\nVelocidadNado=" + VelocidadNado +
+               "\nTipo=" + Tipo + "\n";
     }
-    
 }
