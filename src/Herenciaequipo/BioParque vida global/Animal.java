4a86edfed8fd4790d5f887a20859632ca79db81a
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:34:23 -0600

    Refactor Animal class with improved formatting

--- src/Herenciaequipo/BioParque vida global/Animal.java
+++ src/Herenciaequipo/BioParque vida global/Animal.java
@@ -1,13 +1,17 @@
 package Herenciaequipo;
+
 public class Animal {
-protected String Nombre;
-protected byte Edad;
-protected float Peso;
-protected String Dieta;
-protected String HorarioAlimentacion;
-protected String Sonido;
-public Animal(){}
-public Animal(String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+    protected String Nombre;
+    protected byte Edad;
+    protected float Peso;
+    protected String Dieta;
+    protected String HorarioAlimentacion;
+    protected String Sonido;
+
+    public Animal() {}
+
+    public Animal(String Nombre, byte Edad, float Peso,
+                  String Dieta, String HorarioAlimentacion, String Sonido) {
         this.Nombre = Nombre;
         this.Edad = Edad;
         this.Peso = Peso;
@@ -15,23 +19,32 @@ public Animal(String Nombre, byte Edad, float Peso, String Dieta, String Horario
         this.HorarioAlimentacion = HorarioAlimentacion;
         this.Sonido = Sonido;
     }
-public String getNombre(){return Nombre;}
-public void setNombre(String Nombre){this.Nombre = Nombre;}
-public byte getEdad(){return Edad;}
-public void setEdad(byte Edad){this.Edad = Edad;}
-public float getPeso(){return Peso;}
-public void setPeso(float Peso){this.Peso = Peso;}
-public String getDieta(){return Dieta;}
-public void setDieta(String Dieta){this.Dieta = Dieta;}
-public String getHorarioAlimentacion(){return HorarioAlimentacion;}
-public void setHorarioAlimentacion(String HorarioAlimentacion){this.HorarioAlimentacion = HorarioAlimentacion;}
-public String getSonido(){return Sonido;}
-public void setSonido(String Sonido) {this.Sonido = Sonido;}
-@Override
-public String toString() {
-        return "Animal{" + "Nombre=" + Nombre + ",\nEdad=" + Edad +
-               ", \nPeso=" + Peso + ", \nDieta=" + Dieta +
-               ", \nHorarioAlimentacion=" + HorarioAlimentacion +
-               ",\n Sonido:"+ Sonido+'}';
+
+    public String getNombre(){return Nombre;}
+    public void setNombre(String Nombre){this.Nombre = Nombre;}
+
+    public byte getEdad(){return Edad;}
+    public void setEdad(byte Edad){this.Edad = Edad;}
+
+    public float getPeso(){return Peso;}
+    public void setPeso(float Peso){this.Peso = Peso;}
+
+    public String getDieta(){return Dieta;}
+    public void setDieta(String Dieta){this.Dieta = Dieta;}
+
+    public String getHorarioAlimentacion(){return HorarioAlimentacion;}
+    public void setHorarioAlimentacion(String HorarioAlimentacion){this.HorarioAlimentacion = HorarioAlimentacion;}
+
+    public String getSonido(){return Sonido;}
+    public void setSonido(String Sonido){this.Sonido = Sonido;}
+
+    @Override
+    public String toString() {
+        return "Nombre=" + Nombre +
+               "\nEdad=" + Edad +
+               "\nPeso=" + Peso +
+               "\nDieta=" + Dieta +
+               "\nHorarioAlimentacion=" + HorarioAlimentacion +
+               "\nSonido=" + Sonido;
     }
 }
