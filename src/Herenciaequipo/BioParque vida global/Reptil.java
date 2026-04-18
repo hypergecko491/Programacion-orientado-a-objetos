362009b5c8e8f33e87c5f1829cebfede4867b841
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:44:37 -0600

    Refactor Reptil constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Reptil.java
+++ src/Herenciaequipo/BioParque vida global/Reptil.java
@@ -1,19 +1,20 @@
 package Herenciaequipo;
 public class Reptil extends Animal {
-protected String TipoEscamas;
+    protected String TipoEscamas;
     public Reptil() {}
-    public Reptil(String TipoEscamas, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+    public Reptil(String Nombre, byte Edad, float Peso,
+                  String Dieta, String HorarioAlimentacion, String Sonido,
+                  String TipoEscamas) {
         super(Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
-        this.TipoEscamas = TipoEscamas;}
+        this.TipoEscamas = TipoEscamas;
+    }
     public String getTipoEscamas(){return TipoEscamas;}
-    public void setTipoEscamas(String TipoEscamas) {
-        this.TipoEscamas = TipoEscamas;}
+    public void setTipoEscamas(String TipoEscamas){
+        this.TipoEscamas = TipoEscamas;
+    }
     @Override
     public String toString() {
-        return "Reptil{" +"Nombre=" + Nombre + ", \nEdad=" + Edad + ", "
-                + "\nPeso=" + Peso + ", \nDieta=" + Dieta + ", "
-                + "\nHorarioAlimentacion=" + HorarioAlimentacion + ","
-                + "\nSonido:"+Sonido+ ",\nTipoEscamas=" + TipoEscamas + '}';
+        return super.toString() +
+               "\nTipoEscamas=" + TipoEscamas;
     }
-
 }
