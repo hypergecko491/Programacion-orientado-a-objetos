72cea62ff0259a5afb793186e808c801f04ee53c
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:50:22 -0600

    Refactor Ave class constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Ave.java
+++ src/Herenciaequipo/BioParque vida global/Ave.java
@@ -1,19 +1,18 @@
 package Herenciaequipo;
 public class Ave extends Animal {
-protected float EnvergaduraAlas;
-    public Ave() {
-    }
-    public Ave(float EnvergaduraAlas, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
+    protected float EnvergaduraAlas;
+    public Ave() {}
+    public Ave(String Nombre, byte Edad, float Peso,
+               String Dieta, String HorarioAlimentacion, String Sonido,
+               float EnvergaduraAlas) {
         super(Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
         this.EnvergaduraAlas = EnvergaduraAlas;
     }
-public float getEnvergaduraAlas(){return EnvergaduraAlas;}
-public void setEnvergaduraAlas(float EnvergaduraAlas){this.EnvergaduraAlas = EnvergaduraAlas;}
-@Override
-public String toString() {
-    return "Ave{" +"Nombre=" + Nombre + ", \nEdad=" + Edad +
-           ",\nPeso=" + Peso + ", \nDieta=" + Dieta + ", "+
-           ",\nHorarioAlimentacion=" + HorarioAlimentacion + ",\nSonido:"+Sonido+
-           ",\nEnvergaduraAlas=" + EnvergaduraAlas + '}';
+    public float getEnvergaduraAlas(){return EnvergaduraAlas;}
+    public void setEnvergaduraAlas(float EnvergaduraAlas){this.EnvergaduraAlas = EnvergaduraAlas;}
+    @Override
+    public String toString() {
+        return super.toString() +
+               "\nEnvergaduraAlas=" + EnvergaduraAlas;
     }
 }
