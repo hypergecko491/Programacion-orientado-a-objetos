ff3d606c500c2a2458473c449445121110b114d2
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-04-17 18:48:57 -0600

    Refactor Leon class constructor and toString method

--- src/Herenciaequipo/BioParque vida global/Leon.java
+++ src/Herenciaequipo/BioParque vida global/Leon.java
@@ -1,23 +1,24 @@
 package Herenciaequipo;
 public class Leon extends Mamifero {
-private boolean Melena;
-private float FuerzaMordida;//en newtons
-public Leon(){}
-public Leon(boolean Melena, float FuerzaMordida, String TipoPelaje, String Nombre, byte Edad, float Peso, String Dieta, String HorarioAlimentacion, String Sonido) {
-        super(TipoPelaje, Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido);
+    private boolean Melena;
+    private float FuerzaMordida; // en newtons
+    public Leon() {}
+    public Leon(String Nombre, byte Edad, float Peso,
+                String Dieta, String HorarioAlimentacion, String Sonido,
+                String TipoPelaje,
+                boolean Melena, float FuerzaMordida) {
+        super(Nombre, Edad, Peso, Dieta, HorarioAlimentacion, Sonido, TipoPelaje);
         this.Melena = Melena;
-        this.FuerzaMordida = FuerzaMordida;}
-public boolean isMelena(){return Melena;}
-public void setMelena(boolean Melena){this.Melena = Melena;}
-public float getFuerzaMordida(){return FuerzaMordida;}
-public void setFuerzaMordida(float FuerzaMordida){this.FuerzaMordida = FuerzaMordida;}
-    @Override
-public String toString() {
-        return "Leon{" +"Nombre=" + Nombre + ", \nEdad=" + Edad +
-           ",\nPeso=" + Peso + ", \nDieta=" + Dieta +
-           ",\nHorarioAlimentacion=" + HorarioAlimentacion+
-           ",\nSonido"+Sonido + ",\nTipoPelaje=" + TipoPelaje+ 
-           ",\nMelena=" + Melena + ",\nFuerzaMordida=" + FuerzaMordida + '}';
+        this.FuerzaMordida = FuerzaMordida;
     }
-
+    public boolean isMelena(){return Melena;}
+    public void setMelena(boolean Melena){this.Melena = Melena;}
+    public float getFuerzaMordida(){return FuerzaMordida;}
+    public void setFuerzaMordida(float FuerzaMordida){this.FuerzaMordida = FuerzaMordida;}
+    @Override
+    public String toString() {
+        return "LEON\n" + super.toString() +
+               "\nMelena=" + Melena +
+               "\nFuerzaMordida=" + FuerzaMordida + "\n";
+    }
 }
