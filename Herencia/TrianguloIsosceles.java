297119a3bfe1b6b9bff0e7b1a914ef2b42cd1046
yahir <yahirrip@gmail.com>
2026-04-20 20:18:19 -0600

    Add TrianguloIsosceles class extending Triangulo

---
+++ Herencia/TrianguloIsosceles.java
@@ -0,0 +1,12 @@
+package Herencia;
+public class TrianguloIsosceles extends Triangulo {
+    public TrianguloIsosceles() {
+    }
+    public TrianguloIsosceles(float Base, float Altura, String Color) {
+        super(Base, Altura, Color);
+    }
+    @Override
+    public float CalcularArea() {
+        return super.CalcularArea(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+}
