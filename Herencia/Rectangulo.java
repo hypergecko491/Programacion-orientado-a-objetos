84aa04d65d71271d8eebdd5ea7feb5e7660f519e
yahir <yahirrip@gmail.com>
2026-04-20 20:17:21 -0600

    Clean up Rectangulo.java by removing comments
    Removed unnecessary comments and whitespace.

---
+++ Herencia/Rectangulo.java
@@ -0,0 +1,16 @@
+package Herencia;
+public class Rectangulo extends Poligono{
+
+    public Rectangulo() {
+    }
+
+    public Rectangulo(float Base, float Altura, String Color) {
+        super(Base, Altura, Color);
+    }
+
+    @Override
+    public float CalcularArea() {
+        return super.CalcularArea(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+}
