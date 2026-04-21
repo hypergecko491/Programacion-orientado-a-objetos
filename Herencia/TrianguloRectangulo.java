0f806032573a4a1b5c8731e908045f04c8f4570a
yahir <yahirrip@gmail.com>
2026-04-20 20:19:24 -0600

    Add TrianguloRectangulo class extending Triangulo

---
+++ Herencia/TrianguloRectangulo.java
@@ -0,0 +1,14 @@
+package Herencia;
+public class TrianguloRectangulo extends Triangulo {
+
+    public TrianguloRectangulo() {
+    }
+
+    public TrianguloRectangulo(float Base, float Altura, String Color) {
+        super(Base, Altura, Color);
+    }
+    @Override
+    public float CalcularArea() {
+        return super.CalcularArea(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+}
